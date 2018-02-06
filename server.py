a9656a940a2ae897edc069acb13e2dd3f9bec414
sean185 <seanang185@gmail.com>
2018-02-06 10:49:55 +0000

    abstracted functionality and tidied up code

--- server.py
+++ server.py
@@ -1,51 +1,80 @@
-"""A simple example of a hug API call with versioning"""
+"""Honestbee Data Engineering team interview test"""
+
 import hug
 import json
-import dota2api
 import requests
 from datetime import datetime, timedelta
 
-#api = dota2api.Initialise()
-#match = api.get_match_details(match_id=1000193456)
+INTERVALS = {
+    'week': 7,
+	'month': 30,
+	'year': 365
+}
 
-def api(path, query=None):
+def opendota(path, query=None):
     url = 'https://api.opendota.com/api/' + '/'.join(map(str,path))
     if query is not None:
         url += '?' + '&'.join([k+'='+str(v) for k,v in query.items()])
     res = requests.get(url)
+    print(url)
     return json.loads(res.content.decode('utf-8'))
 
-#@hug.get('/match')
-#def match(id):
-#    return api.get_match_details(match_id=id)
+def resolve(player):
+    if player.isdigit():
+        return player
+    else:
+        data = opendota( ('search'), {'q': player, 'similarity': 1} )
+        ## TODO: catch when there are no players returned
+        return data[0]['account_id']
 
+HEROES = None
+def heroes():
+    global HEROES
+    if HEROES is not None:
+        return HEROES
+    data = opendota( ('heroes',) )
+    HEROES = { str(hero['id']): hero['localized_name'] for hero in data }
+    print('preloaded heroes')
+    # print(HEROES)
+    return HEROES
+
+## Question 1
 @hug.get('/leaderboard')
-def comparison(player_ids: hug.types.multiple, time_frame: hug.types.text):
+def leaderboard(player_ids: hug.types.multiple, time_frame: hug.types.text = 'week'):
     board = []
     ## handle if personaname is given instead of account_id
-    for player_id in player_ids:
-        if player_id.isdigit():
-            board.append({'account_id': player_id})
-        else:
-            data = api( ('search'), {'q': player_id, 'similarity': 1} )
-            board.append({'account_id': data[0]['account_id']})
-    now = datetime.now()
-    if time_frame == 'week':
-        days = 7
-    if time_frame == 'month':
-        days = 30
-    if time_frame == 'year':
-        days = 365
-    for player in board:
-        data = api( ('players', player['account_id'], 'wl'), {'date': days} )
+    players = [resolve(player_id) for player_id in player_ids]
+    days = INTERVALS[time_frame]
+    # now = datetime.now()
+    for player in players:
+        data = opendota( ('players', player, 'wl'), {'date': days} )
         wins = data['win']
-        player['wins'] = wins
-        player['win_rate'] = float(wins)/days
-    #subset = filter(lambda x: datetime.fromtimestamp(x['start_time']) >= now - timedelta(days=days), res['matches'])
-    #return subset
-    # return map(lambda x: x['match_id'], res['matches'])
-    return board
+        board.append({'account_id': player, 'wins': wins, 'win_rate': wins/days})
+    return sorted(board, key=lambda x: -x['win_rate'])
 
-@hug.get('/unversioned')
-def hello():
-    return 'Hello world!'
+## Question 2
+@hug.get('/compare')
+def compare(player_A: hug.types.text, player_B: hug.types.text):
+    A = get_stats(player_A)
+    B = get_stats(player_B)
+    result = {}
+    for A_stat, B_stat in zip(A, B):
+        if A_stat[1] > B_stat[1]:
+            result[A_stat[0]] = player_A
+        else:
+            result[A_stat[0]] = player_B
+    return result
+
+def get_stats(player):
+    player = resolve(player)
+    data = opendota( ('players', player, 'totals') )
+    ## want kda, gpm, xpm [3:6]
+    return map(lambda stat: (stat['field'], stat['sum']/stat['n'] if stat['n'] > 0 else 0), data)
+
+## Question 3
+@hug.get('/suggest')
+def suggest(player: hug.types.text):
+    player = resolve(player)
+    data = opendota( ('players', player, 'heroes') )
+    best_hero = max(data, key=lambda h: h['win']/h['games'] if h['games'] > 0 else 0)
+    return heroes()[best_hero['hero_id']]
